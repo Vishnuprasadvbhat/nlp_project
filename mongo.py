b44531632cbd9308277e3490cf63c0152998b4b5
wargod <vishnuprasadvbhat@gmail.com>
2024-09-11 01:10:12 +0530

    started with mongo

---
+++ mongo.py
@@ -0,0 +1,12 @@
+# import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://vishnuprasadvbhat:p5OafHBJZQc3vZ3A@amazonreview.3kiqf.mongodb.net/?retryWrites=true&w=majority&appName=AmazonReview")
+
+db = cluster['Amazon']
+collections = db['User']
+
+data = {"UserId" : 1, "Name": "Vishnuprasad", "Password": 1234, "Address" : "Mangalore", "Phone No": 123456789}
+
+
+collections.insert_one(data)
